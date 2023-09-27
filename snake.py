0ed77089e09d6eaeb1d2c06c522dd13dbc57a888
Valenciola <vfilsaime2021@fau.edu>
2023-09-27 21:18:37 +0000

    Cringe coding :D

---
+++ snake.py
@@ -0,0 +1,6 @@
+print ("I can write a line in python")
+print ("Can't forget the not semicolon")
+x = 3
+print ("I can write a variable where x is", x)
+# Oh what it put the space for me two with the comma
+print ("This is too easy something must be wrong-")
