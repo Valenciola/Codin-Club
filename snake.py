2d37e58fdb654b0a3b42c73324ff6e4d9d4b3ca2
Valenciola <vfilsaime2021@fau.edu>
2023-10-05 17:03:19 +0000

    Trying to keep my program from deleting itself

--- snake.py
+++ snake.py
@@ -4,3 +4,4 @@ x = 3
 print ("I can write a variable where x is", x)
 # Oh what it put the space for me two with the comma
 print ("This is too easy something must be wrong-")
+# Availability comment
